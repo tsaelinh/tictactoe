0998ac32d0bfe024c3741532fb68728592b91e6e
Tiffany Saelinh <saelinht@gmail.com>
2021-02-23 18:28:57 -0800

    Got junit test working

--- src/board.java
+++ src/board.java
@@ -1,3 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class board {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public int sum(int x, int y){
+        return x + y;
+    }
+
+    @Test
+    public void testSum(){
+        assertEquals(5, sum(3, 2));
+    }
 }
